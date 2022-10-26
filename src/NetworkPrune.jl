4c5221b935089ca533c3f1146da35729b703e0d1
Manuel <chacarero@gmail.com>
2022-10-26 14:35:21 +0200

    Add main API

--- src/NetworkPrune.jl
+++ src/NetworkPrune.jl
@@ -1,5 +1,14 @@
 module NetworkPrune
 
-# Write your package code here.
+using SpineOpt
+using SpineInterface
+using PowerSystems
+using PowerModels
+
+export psse_to_spine
+export prune_network
+
+include("psse_to_spine.jl")
+include("prune_network.jl")
 
 end
